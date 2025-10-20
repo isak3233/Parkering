7505be98df7057cdc0f4b79419a7d89d1e4a49d6
isak3233 <isak.heimdahl@gmail.com>
2025-10-20 11:23:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Parkering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car1 = new("ABC123");
+            Console.WriteLine(car1.LicensePlate);
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkering
+{
+    internal class Vehicle
+    {
+        public string LicensePlate {  get;  }
+        public Vehicle(string licensePlate)
+        {
+            LicensePlate = licensePlate;
+        }
+    }
+}
