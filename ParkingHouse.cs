3da2562dd114e169d6f401bd77da0ed261bc5bf6
isak3233 <isak.heimdahl@gmail.com>
2025-10-20 12:15:47 +0200

    Gjorde om så att vi har nu en lista på parkingslots och gjorde så vi kunde skriva ut det

--- ParkingHouse.cs
+++ ParkingHouse.cs
@@ -8,24 +8,30 @@ namespace Parkering
 {
     internal class ParkingHouse
     {
-        public Vehicle[] ParkingSpots { get; set; }
+        public List<ParkingSpot> ParkingSpots { get; set; }
         public ParkingHouse(int amountOfParkings)
         {
-            ParkingSpots = new Vehicle[amountOfParkings];
-            ParkingSpots[0] = new Car("ABC123", Colors.Red, false);
-            ParkingSpots[1] = new Car("DFG321", Colors.Blue, true);
-            ParkingSpots[2] = new Motorcycle("AAA111", Colors.Green, "Volkswagen");
+            ParkingSpots = new List<ParkingSpot>();
+            for (int i = 0; i < amountOfParkings; i++)
+            {
+                ParkingSpots.Add(new ParkingSpot());
+            }
+
+
+            ParkingSpots[0].AddVehicle(new Car("ABC123", Colors.Red, false));
+            ParkingSpots[1].AddVehicle(new Car("DFG321", Colors.Blue, true));
+            ParkingSpots[2].AddVehicle(new Motorcycle("AAA111", Colors.Green, "Volkswagen"));
         }
         public void WriteOutParking()
         {
             int index = 1;
-            foreach(Vehicle vehicle in ParkingSpots)
+            foreach(ParkingSpot parkingSpot in ParkingSpots)
             {
-                if(vehicle != null)
+                foreach (Vehicle vehicle in parkingSpot.Vehicles)
                 {
                     Console.WriteLine($"{index}\t {vehicle.GetInformation()}");
+                    
                 }
-                
                 index++;
             }
         }
---
+++ ParkingSpot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkering
+{
+    internal class ParkingSpot
+    {
+        public List<Vehicle> Vehicles { get; set; }
+        public ParkingSpot()
+        {
+            Vehicles = new List<Vehicle>();
+        }
+        public void AddVehicle(Vehicle vehicleToAdd)
+        {
+            Vehicles.Add(vehicleToAdd);
+        }
+        public void RemoveVehicle(Vehicle vehicleToRemove)
+        {
+            Vehicles.Remove(vehicleToRemove);
+        }
+    }
+}
